a80ba106c2907b6d9ce5933146203f0686685c7f
Youssef Skandrani <youssef.skandrani@outlook.com>
2021-04-17 19:40:14 +0200

    second change

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your VueApp !!"/>
+    <HelloWorld msg="Welcome to Your VueApp Ya Miboun !!"/>
   </div>
 </template>
 
